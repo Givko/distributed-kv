018a702a52f8d23705cecbf8aa9df8ebf91de2bb
Givko <milevjivko@gmail.com>
2026-01-07 20:18:11 +0200

    finish leader election with locks

--- raft/src/main.rs
+++ raft/src/main.rs
@@ -32,31 +32,22 @@ pub struct RaftService {
     elect_timer_reset_tx: Sender<()>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Default)]
 enum State {
-    Candidate {
-        votes: usize,
-    },
+    Candidate,
     Leader,
-    Follower {
-        leader: String,
-    },
+    #[default]
+    Follower,
 }
 
-impl Default for State {
-    fn default() -> Self {
-        State::Follower {
-            leader: String::new(),
-        }
-    }
-    
-}
-
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct Node {
     current_term: usize,
     state: State,
     peers: Vec<String>,
+    voted_for: bool, 
+    elect_timer_reset_tx: Sender<()>,
+    election_handle: Option<JoinHandle<()>>,
 }
 
 impl Node {
@@ -65,17 +56,11 @@ impl Node {
     }
 
     fn is_candidate(&self) -> bool {
-        match self.state {
-            State::Candidate { .. } => true,
-            _ => false,
-        }
+        self.state == State::Candidate
     }
 
     fn is_follower(&self) -> bool {
-        match self.state {
-            State::Follower { .. } => true,
-            _ => false,
-        }
+        self.state == State::Follower
     }
 }
 
@@ -87,26 +72,65 @@ impl Raft for RaftService {
     ) -> Result<Response<RequestVoteReply>, Status> {
         eprintln!("Got requestVote");
 
-        let vote = RequestVoteReply {
-            term: 1,
+        let vote_request = request.into_inner();
+
+        let mut node_guard = self.node.write().await;
+        if node_guard.current_term < vote_request.term as usize {
+            node_guard.current_term = vote_request.term as usize;
+            node_guard.voted_for = false;
+            node_guard.state = State::Follower;
+        }
+
+        if node_guard.voted_for || node_guard.current_term > vote_request.term as usize {
+            let vote = RequestVoteReply {
+                term: node_guard.current_term as u64,
+                vote: false,
+            };
+
+            return Ok(Response::new(vote));
+        }
+
+        let vote_reply = RequestVoteReply {
+            term: node_guard.current_term as u64,
             vote: true,
         };
 
-        Ok(Response::new(vote))
+        node_guard.current_term = vote_request.term as usize;
+        node_guard.voted_for = true;
+        node_guard.state = State::Follower;
+
+        Ok(Response::new(vote_reply))
     }
 
     async fn append_entries(
         &self,
         request: Request<AppendEntriesMessage>,
     ) -> Result<Response<AppendEntriesReply>, Status> {
-        eprintln!("got append entries");
+        let reply = request.into_inner();
+        let mut node_guard = self.node.write().await;
+        if node_guard.current_term > reply.term as usize {
+            let vote = AppendEntriesReply {
+                term: node_guard.current_term as u64,
+                success: false,
+            };
+
+            return Ok(Response::new(vote));
+        }
 
         let reply = AppendEntriesReply {
-            term: 1,
+            term: node_guard.current_term as u64,
             success: true,
         };
 
         self.elect_timer_reset_tx.send(()).await.unwrap();
+        node_guard.state = State::Follower;
+
+        // Reset voted_for on receiving heartbeat
+
+        if node_guard.current_term < reply.term as usize {
+            node_guard.current_term = reply.term as usize;
+            node_guard.voted_for = false;
+        }
 
         Ok(Response::new(reply))
     }
@@ -118,6 +142,9 @@ impl RaftService {
             current_term: 0,
             state: State::default(),
             peers,
+            voted_for: false,
+            elect_timer_reset_tx: snd.clone(),
+            election_handle: None,
         };
 
         RaftService {
@@ -134,21 +161,58 @@ async fn election_timer(node: Arc<RwLock<Node>>, mut reset_rx: Receiver<()>) {
 
         if node.read().await.is_leader() {
             // If we're the leader, we don't need to run the election timer
-            eprintln!("Node is leader, skipping election timer");
-            tokio::time::sleep(Duration::from_millis(100)).await;
+            //TODO send heartbeat messages
+            tokio::time::sleep(Duration::from_millis(50)).await;
+            for peer in node.read().await.peers.clone() {
+                if let Ok(mut peer_client) = RaftClient::connect(format!("http://{}", peer)).await {
+                    let node = node.clone();
+                    tokio::spawn(async move {
+                        let append_entries_request = Request::new(AppendEntriesMessage {
+                            term: node.read().await.current_term as u64,
+                        });
+
+                        if let Ok(reply) = peer_client.append_entries(append_entries_request).await {
+                            let reply_inner = reply.into_inner();
+                            if reply_inner.success {
+                                eprintln!("Heartbeat acknowledged by {}", peer);
+                                return;
+                            } 
+
+                            eprintln!("Heartbeat rejected by {}", peer);
+                            if reply_inner.term as usize > node.read().await.current_term {
+                                let mut node_guard = node.write().await;
+                                node_guard.current_term = reply_inner.term as usize;
+                                node_guard.state = State::Follower;
+                                eprintln!("Stepping down to follower due to higher term from {}", peer);
+                            }
+                        } 
+                    });
+                }
+            }
+
             continue;
         }
 
         tokio::select! {
             _ = tokio::time::sleep(timeout_duration) => {
                 // Election timeout elapsed, start a new election
-                //eprintln!("Election timeout elapsed, starting new election");
+                if node.read().await.is_candidate() {
+                    eprintln!("Node became candidate during timeout, aborting election");
+                    if let Some(handle) = node.write().await.election_handle.take() {
+                        handle.abort();
+                    }
+                }
+
+                let node_clone = node.clone();
+                let election_handle = tokio::spawn(async move {
+                    election_process(node_clone).await;
+                });
+
+                let mut node_guard = node.write().await;
+                node_guard.election_handle = Some(election_handle);
             }
             _ = reset_rx.recv() => {
                 // Received a reset signal, restart the timer
-                eprintln!("Election timer reset");
-                //election_process(node.clone()).await;
-                election_process(node.clone()).await;
                 continue;
             }
         }
@@ -161,10 +225,11 @@ async fn election_process(node: Arc<RwLock<Node>>) {
     {
         let mut node_guard = node.write().await;
         node_guard.current_term += 1;
-        node_guard.state = State::Candidate { votes: 1 };
+        node_guard.state = State::Candidate; 
+        node_guard.voted_for = true;
     }
 
-    //TODO: Send requestVote to all other nodes
+    eprintln!("Current node state: {:?}", node.read().await.state);
     let mut join_handles: Vec<JoinHandle<Result<Response<RequestVoteReply>, Status>>> = vec![];
     let cur_term = node.read().await.current_term;
 
@@ -172,22 +237,24 @@ async fn election_process(node: Arc<RwLock<Node>>) {
     let peers = node.read().await.peers.clone();
     for peer in peers {
         eprintln!("Sending requestVote to {}", peer);
-        let mut peer_client = RaftClient::connect(format!("http://{}", peer)).await.expect("Failed to connect to peer");
-        let vote_request = Request::new(RequestVoteMessage {
-            term: cur_term as u64,
-            candidate_id: 1,
-        });
+        if let Ok(mut peer_client) = RaftClient::connect(format!("http://{}", peer)).await {
+            let vote_request = Request::new(RequestVoteMessage {
+                term: cur_term as u64,
+            });
 
-        let handle: JoinHandle<Result<Response<RequestVoteReply>, Status>> = tokio::spawn(async move {
-            let vote_reply = peer_client.request_vote(vote_request).await;
-            eprintln!("Got vote reply from {}: {:?}", peer, vote_reply);
-            vote_reply
-        });
+            let handle: JoinHandle<Result<Response<RequestVoteReply>, Status>> = tokio::spawn(async move {
+                let vote_reply = peer_client.request_vote(vote_request).await;
+                eprintln!("Got vote reply from {}: {:?}", peer, vote_reply);
+                vote_reply
+            });
 
-        join_handles.push(handle);
+            join_handles.push(handle);
+        }
     }
 
     let mut votes = vec![];
+
+    //TODO: this is sequential, make it parallel use channels
     for handle in join_handles {
         match handle.await {
             Ok(Ok(vote_reply)) => {
@@ -202,22 +269,37 @@ async fn election_process(node: Arc<RwLock<Node>>) {
         }
     }
     
-    // Before counting votes, check if we became leader already
-    if node.read().await.is_leader() || node.read().await.is_follower() {
-        eprintln!("Node became leader or follower during election, aborting election process");
-        return;
-    }
-
-    //TODO: Count votes and become leader if majority is reached
+    eprintln!("cur state after votes collected: {:?}", node.read().await.state);
+    let mut votes_recieved = 1; // vote for self
     for vote in votes {
         eprintln!("Vote: {:?}", vote);
+        if vote.vote {
+            votes_recieved += 1;
+        }
     }
 
-    {
-        // take write lock
-        // count votes
-        // if majority, become leader
+    eprintln!("Total votes received: {}", votes_recieved);
+    if votes_recieved <= (node.read().await.peers.len() + 1) / 2 {
+        eprintln!("Did not receive majority votes, remaining candidate");
+        return;
     }
+    
+    {
+        // Get write lock in order to prevent state update while updating state
+        let mut node_guard = node.write().await;
+        eprintln!("cur state before becoming leader: {:?}", node_guard.state);
+        if node_guard.is_leader() || node_guard.is_follower() {
+            eprintln!("Node became leader or follower during election, aborting election process");
+            return;
+        }
+
+        eprintln!("Received majority votes, becoming leader");
+        node_guard.state = State::Leader;
+    }
+
+    // Reset election timer
+    node.read().await.elect_timer_reset_tx.send(()).await.expect("Failed to send reset signal");
+    return;
 }
 
 #[tokio::main]
