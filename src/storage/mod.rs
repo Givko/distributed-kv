00759b78bfe7907c33d78df94353079e5981920a
Givko <milevjivko@gmail.com>
2026-03-24 00:12:37 +0200

    add bloom filter struct

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,8 +1,9 @@
+pub(super) mod bloom_filter;
 pub(super) mod encoder;
 pub(super) mod entry;
 pub(super) mod fs;
+pub(super) mod memtable;
 pub(super) mod sstable;
 pub(super) mod wal;
-pub(super) mod memtable;
 
 pub mod lsm_tree;
